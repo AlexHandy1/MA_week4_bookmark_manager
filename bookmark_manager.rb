597600bf98606e04c5b600ccba4acfba988602d4
MollieS <mollie.c.stephenson@gmail.com>
2015-05-18 17:13:02 +0100

    Password confirmation added

--- bookmark_manager.rb
+++ bookmark_manager.rb
@@ -15,6 +15,8 @@ DataMapper.auto_upgrade!
 
 class BookmarkManager < Sinatra::Base
   set :views, proc { File.join(root,'views') }
+  enable :sessions
+  set :session_secret, 'super secret'
 
   get '/' do
     @links = Link.all
@@ -42,10 +44,20 @@ class BookmarkManager < Sinatra::Base
   end
 
   post '/users' do
-    User.create(email: params[:email],
-                 password: params[:password])
+    user = User.create(email: params[:email],
+                 password: params[:password],
+                 password_confirmation: params[:password_confirmation])
+    session[:user_id] = user.id
     redirect to('/')
   end
+
+    helpers do
+
+      def current_user
+        @current_user ||= User.get(session[:user_id]) if session[:user_id]
+      end
+
+    end
 
   run! if app_file == $0
 end
--- lib/user.rb
+++ lib/user.rb
@@ -1,4 +1,5 @@
 require 'data_mapper'
+require 'bcrypt'
 
 class User
 
@@ -6,6 +7,16 @@ class User
 
   property :id, Serial
   property :email, String
-  property :password, String
+  property :password_digest, Text
+
+  attr_reader :password
+  attr_accessor :password_confirmation
+
+  validates_confirmation_of :password
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
 
 end
